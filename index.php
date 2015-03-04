6bc13b98a6cde1b24cec1008ebacf01ee251d1a8
fellipedoprado <fellipe.doprado@gmail.com>
2015-03-04 18:47:15 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include 'header.php';
+//include_once 'connection.php';
+
+//$query = "SELECT PID,ACCID,OID FROM orders";
+//$cols = array("PID","ACCID","OID","orderdate");
+
+//retrieveRecords($query, $cols, " orders");
+echo <<<_END
+<html> 
+<body>
+		
+<p>information and other stuff here</p>
+
+
+</body>
+</html>
+_END;
+include 'footer.php';		
+?>

ece3456b5bd44f6344d962503f0662ea0def0b3b
fellipedoprado <fellipe.doprado@gmail.com>
2015-03-04 18:45:15 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,53 @@
+<?php
+?>
+<!--  My Site menu goes here........... Choice 1 | Choice 2 | Choice 3 -->
+<html>
+ <head><title>Sailers</title></head>
+ <body>
+ <body bgcolor="#CCFFFF">
+ <h2><Strong>Sailers</Strong></h2><p><br>
+ <img src="images/logo.jpg" alt="Sailers" style="width:1000x;height:200px"><br> 
+ 
+ 
+
+  <p><a href="signInPage.php">sign in</a></p>
+  <p><a href="newUserPage.php">new user</a></p>
+ 
+
+
+ 
+ 
+ 
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+
+li {
+    float: left;
+}
+
+a {
+    display: block;
+    width: 100px;
+    background-color: #E8E8EE;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="">Link</a></li>
+  <li><a href="">Link</a></li>
+  <li><a href="">Link</a></li>
+</ul>
+ <hr>
+ 
+ 

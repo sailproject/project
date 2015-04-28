14bbe0fee1f1f540a3a89230f046e5bbd355ecbf
virginiavandyck <vrvandyck42@gmail.com>
2015-04-28 10:08:08 -0400

    Update newUserPage.php

--- newUserPage.php
+++ newUserPage.php
@@ -15,3 +15,53 @@ Repassword: <input type="password" name="repassword"><br>
 _END;
 
 ?>
+<script>
+
+isValidName=true;
+isValidPwd=true;
+idName=document.getElementById("nameID");
+idName.onblur=function()
+{
+	var errorLabel=document.getElementById("eMName");
+	if(idName.value=="")
+	{
+		errorLabel.innerHTML="Username cannot be empty.";
+		isValidName=false;
+	}
+	else if(/[^a-zA-Z0-9-_]/.test(idName.value))
+	{
+		errorLabel.innerHTML="Username can only have letters, numbers, hyphen, or underscore";
+		isValidName=false;
+	}
+	else
+	{
+		errorLabel.innerHTML="";
+		isValidName=true;
+	}
+}
+
+idPwd=document.getElementById("pwdID");
+idPwd.onblur=function()
+{
+	var errorLabel=document.getElementId("eMPwd");
+	if((idPwd.value).length < 5)
+	{
+		errorLabel.innerHTML="Password must be at least 5 characters";
+		isValidPwd=false;
+	}
+	else
+	{
+		errorLabel.innerHTML="";
+		isValidPwd=true;
+	}
+}
+
+function allValid()
+{
+	if(isValidName && isValidPwd)
+		return true;
+	else
+		return false;
+}
+
+</script>
